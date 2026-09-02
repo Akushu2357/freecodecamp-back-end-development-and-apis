09f546bd1e0d30294b672aa3a65c3854c426313b
Akushu2357 <tang.tang2004@gmail.com>
2026-09-02 16:36:41 +0000

    submitted: prime number checker module

---
+++ build-a-prime-number-checker-module/index.js
@@ -0,0 +1,13 @@
+const isPrime = (num) => {
+    if (num <= 1) { return false; }
+    for (let index = 2; index <= Math.trunc(Math.sqrt(num)); index++) {
+        if (num % index == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports = {
+    isPrime
+};
